4739d6d9b33253b188ebf95f41edbc4000255f99
user <bipinaghimire6@gmail.com>
2021-08-24 19:42:58 +0545

    Second commit

--- main.py
+++ main.py
@@ -13,15 +13,16 @@ root.geometry('1500x900')
 #         first_name  text ,
 #        last_name  text ,
 #         age integer ,
-#         address text ,
-#         date_of_birth integer ,
 #         gender text ,
+#         date_of_birth integer,
+#         Class integer,
+#         Contact_number integer,
+#          Email_address text,
+#         address text ,
 #          Father_name text ,
 #         Mother_name text ,
-#         Blood_group text ,
-#         Contact_number integer ,
-#          Email_address text,
-#       marital status text
+#         Complaints
+
 #      )'''))
 
 # title
@@ -37,66 +38,71 @@ Entry_frame_details = Frame(root, bd=10, width=1250, height=900, padx=250, relie
 Entry_frame_details.pack(side=BOTTOM)
 
 # create textLabels
-first_name_label = Label(Entry_frame_details, text='First Name', font='Cambria 15 italic').place(x=-250,y=15)
+first_name_label = Label(Entry_frame_details, text='First Name', font='Cambria 15 italic').place(x=-250,y=55)
 
-Middle_name_label = Label(Entry_frame_details, text='Middle Name', font='Cambria 15 italic').place(x=200,y=15)
+Middle_name_label = Label(Entry_frame_details, text='Middle Name', font='Cambria 15 italic').place(x=200,y=55)
 
-last_name_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Last Name').place(x=600,y=15)
+last_name_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Last Name').place(x=600,y=55)
 
-class_label = Label(Entry_frame_details, text='Class', font='Cambria 15 italic').place(x=550,y=350)
+class_label = Label(Entry_frame_details, text='Class', font='Cambria 15 italic').place(x=-250,y=105)
 
-age_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Age').place(x=-250,y=65)
-
-
-address_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Address').place(x=250,y=65)
+age_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Age').place(x=90,y=105)
 
 
-date_of_birth_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Date Of Birth ').place(x=-250,y=110)
+address_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Address').place(x=-250,y=160)
 
 
-gender_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Gender').place(x=250,y=110)
+date_of_birth_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Date Of Birth(*in AD)').place(x=520,y=105)
 
 
-father_name_label = Label(Entry_frame_details, font='Cambria 15 italic', text="Father's name ").place(x=250,y=160)
+gender_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Gender').place(x=290,y=105)
 
 
-mother_name_label = Label(Entry_frame_details, font='Cambria 15 italic', text="Mother's name ").place(x=-250,y=160)
+father_name_label = Label(Entry_frame_details, font='Cambria 15 italic', text="Father's name ").place(x=185,y=210)
 
 
-Contact_number_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Contact Number').place(x=250,y=210)
+mother_name_label = Label(Entry_frame_details, font='Cambria 15 italic', text="Mother's name ").place(x=-250,y=210)
 
-Email_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Email address').place(x=-250,y=210)
+
+Contact_number_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Contact Number').place(x=600,y=210)
+
+Email_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Email address').place(x=365,y=160)
 
 Date_of_admission_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Date of admission').place(x=-250,y=260)
 
-Previous_school_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Previous School (*optional)').place(x=-250,y=310)
+complain_label = Label(Entry_frame_details, font='Cambria 15 italic', text='Complaints ').place(x=250,y=260)
 
 # create textboxes
 
-first_name = Entry(Entry_frame_details, font='Cambria 15 italic', width=22, borderwidth=5).place(x=-120,y=15)
+first_name = Entry(Entry_frame_details, font='Cambria 15 italic', width=22, borderwidth=5).place(x=-120,y=55)
 
-Middle_name = Entry(Entry_frame_details, font='Cambria 15 italic', width=22, borderwidth=5).place(x=330,y=15)
+Middle_name = Entry(Entry_frame_details, font='Cambria 15 italic', width=22, borderwidth=5).place(x=330,y=55)
 
-last_name = Entry(Entry_frame_details, font='Cambria 15 italic', width=22, borderwidth=5).place(x=720,y=15)
+last_name = Entry(Entry_frame_details, font='Cambria 15 italic', width=22, borderwidth=5).place(x=720,y=55)
 
-age = Entry(Entry_frame_details, font='Cambria 15 italic', width=20, borderwidth=5).place(x=-120,y=65)
+Class = Entry(Entry_frame_details, font='Cambria 15 italic', width=13, borderwidth=5).place(x=-120,y=105)
 
-address = Entry(Entry_frame_details, font='Cambria 15 italic', width=20, borderwidth=5)
+age = Entry(Entry_frame_details, font='Cambria 15 italic', width=9, borderwidth=5).place(x=150,y=105)
 
-date_of_birth = Entry(Entry_frame_details, font='Cambria 15 italic', width=20, borderwidth=5)
+address = Entry(Entry_frame_details, font='Cambria 15 italic', width=36, borderwidth=5).place(x=-120,y=160)
 
-gender = Entry(Entry_frame_details, font='Cambria 15 italic', width=20, borderwidth=5)
+date_of_birth = Entry(Entry_frame_details, font='Cambria 15 italic', width=22, borderwidth=5).place(x=720,y=105)
 
-father_name = Entry(Entry_frame_details, font='Cambria 15 italic', width=20, borderwidth=5)
+gender = Entry(Entry_frame_details, font='Cambria 15 italic', width=7, borderwidth=5).place(x=390,y=105)
 
-mother_name = Entry(Entry_frame_details, font='Cambria 15 italic', width=20, borderwidth=5)
+father_name = Entry(Entry_frame_details, font='Cambria 15 italic', width=22, borderwidth=5).place(x=325,y=210)
+
+mother_name = Entry(Entry_frame_details, font='Cambria 15 italic', width=22, borderwidth=5).place(x=-100,y=210)
 
 
-Contact_number = Entry(Entry_frame_details, font='Cambria 15 italic', width=20, borderwidth=5)
+Contact_number = Entry(Entry_frame_details, font='Cambria 15 italic', width=18, borderwidth=5).place(x=763,y=210)
 
 
-Email_address = Entry(Entry_frame_details, font=('Cambria 15 italic', 14,), width=20, borderwidth=5)
+Email_address = Entry(Entry_frame_details, font=('Cambria 15 italic', 14,), width=40, borderwidth=5).place(x=522,y=160)
 
+Date_of_admission = Entry(Entry_frame_details, font='Cambria 15 italic', width=22, borderwidth=5).place(x=-80,y=260)
+
+complain = Entry(Entry_frame_details, font='Cambria 15 italic', width=53, borderwidth=5).place(x=380,y=260)
 
 #conn.commit()
 #conn.close()
